bdd4428ba7a437c045f11eda451c37b53ee7c1fa
DanichMan <53317146+DraimCiDo@users.noreply.github.com>
2021-11-15 23:55:42 +0200

    Перевод настроек

--- src/main/java/com/lenis0012/bukkit/marriage2/config/Settings.java
+++ src/main/java/com/lenis0012/bukkit/marriage2/config/Settings.java
@@ -12,7 +12,7 @@ public class Settings {
      */
     public static final ConfigOption<Integer> REQUEST_EXPIRY = new ConfigOption<>("requestExpiry", 60);
     public static final ConfigOption<Boolean> ENABLE_PRIEST = new ConfigOption<>("enable-priests", false);
-    @ConfigHeader("Allow a player's gender to be changed multiple times.")
+    @ConfigHeader("Разрешите несколько раз менять пол игрока.")
     public static final ConfigOption<Boolean> ALLOW_GENDER_CHANGE = new ConfigOption<>("enable-gender-change", true);
 
     /**
@@ -23,25 +23,25 @@ public class Settings {
     /**
      * Features
      */
-    @ConfigHeader("Show gender colors in marry list command.")
+    @ConfigHeader("Показывать цвета в списке женатых.")
     public static final ConfigOption<Boolean> GENDER_IN_LIST = new ConfigOption<>("features.genders-in-list", true);
 
     /**
      * Chat
      */
     @ConfigHeader(path = "chat", value = {
-            "Chat, set the format of private messages and in-chat status.",
-            "Supported tags for chat: {partner}, for pm: {name}, {message}, chat spy: {sender}, {receiver}, {message}",
-            "Icons are always available: {icon:heart}, {icon:male}, {icon:female}, {icon:genderless}",
-            "If you use a custom chat plugin, put {marriage_status} in the format and set force-status-format & force-gender-format to false",
-            "To show genders in chat, put {marriage_gender} in chat plugin format"
+            "Это чат, установите формат личных сообщений и статус в чате.",
+            "Поддерживаемые теги для чата: {partner}, для лс: {name}, {message}, для слежки: {sender}, {receiver}, {message}",
+            "Доступные значки: {icon:heart}, {icon:male}, {icon:female}, {icon:genderless}",
+            "Если вы используете пользовательский плагин чата, поместите {marriage_status} в формат и установите force-status-format & force-gender-format на false",
+            "Чтобы показать пол в чате, поставьте {marriage_gender} в формата вашего чата"
     })
     public static final ConfigOption<String> PM_FORMAT = new ConfigOption<>("chat.pm-format", "&4{icon:heart}&c{name}&4{icon:heart} &7{message}");
-    public static final ConfigOption<String> CHAT_FORMAT = new ConfigOption<>("chat.status-format", "&4&l<3 &r");
+    public static final ConfigOption<String> CHAT_FORMAT = new ConfigOption<>("chat.status-format", "&4&l♥ &r");
     public static final ConfigOption<String> CHAT_FORMAT_UNMARRIED = new ConfigOption<>("chat.unmarried-status-format", "");
     public static final ConfigOption<String> CHATSPY_FORMAT = new ConfigOption<>("chat.spy-format", "&c[CHAT SPY] &7{sender} -> {receiver}&f: {message}");
-    public static final ConfigOption<Boolean> FORCE_FORMAT = new ConfigOption<>("chat.force-status-format", true);
-    public static final ConfigOption<Boolean> FORCE_GENDER_FORMAT = new ConfigOption<>("chat.force-gender-format", true);
+    public static final ConfigOption<Boolean> FORCE_FORMAT = new ConfigOption<>("chat.force-status-format", false);
+    public static final ConfigOption<Boolean> FORCE_GENDER_FORMAT = new ConfigOption<>("chat.force-gender-format", false);
     public static final ConfigOption<String> PREFIX_MALE = new ConfigOption<>("chat.male-prefix", "&b{icon:male} &r");
     public static final ConfigOption<String> PREFIX_FEMALE = new ConfigOption<>("chat.female-prefix", "&d{icon:female} &r");
     public static final ConfigOption<String> PREFIX_GENDERLESS = new ConfigOption<>("chat.genderless-prefix", "");
@@ -50,8 +50,8 @@ public class Settings {
      * Kissing
      */
     @ConfigHeader(path = "kisses", value = {
-            "Kissing, display hearts when 2 married players kiss eachother.",
-            "The amount of hearts is a random number between min and max."
+            "Поцелуи, показывают сердечки, когда 2 женатых игрока целуют друг друга.",
+            "Количество сердец - это случайное число от минимального до максимального."
     })
     public static final ConfigOption<Boolean> KISSES_ENABLED = new ConfigOption<>("kisses.enabled", true);
     public static final ConfigOption<Integer> KISSES_AMOUNT_MIN = new ConfigOption<>("kisses.amount-min", 5);
@@ -60,17 +60,17 @@ public class Settings {
     /**
      * XP-Boost
      */
-    @ConfigHeader(path = "exp-boost", value = "Boost EXP gained when married players are near each other.")
+    @ConfigHeader(path = "exp-boost", value = "Увеличение опыта, которые получают игроки, когда они находятся рядом друг с другом.")
     public static final ConfigOption<Boolean> EXP_BOOST_ENABLED = new ConfigOption<>("exp-boost.enabled", true);
     public static final ConfigOption<Double> EXP_BOOST_DISTANCE = new ConfigOption<>("exp-boost.distance", 50.0);
     public static final ConfigOption<Double> EXP_BOOST_MULTIPLIER = new ConfigOption<>("exp-boost.multiplier", 2.0);
-    @ConfigHeader("Announces amount of bonus EXP gaines, set to false to disable.")
+    @ConfigHeader("Сумма бонуса, увеличивающего опыт, установите значение false, чтобы отключить.")
     public static final ConfigOption<Boolean> EXP_BOOST_ANNOUNCE = new ConfigOption<>("exp-boost.announce", true);
 
     /**
      * Support
      */
-    @ConfigHeader("Automatically trust married players to each others plot.")
+    @ConfigHeader("Подхват Plotsqueard.")
     public static final ConfigOption<Boolean> PLOTSQUARED_AUTO_TRUST = new ConfigOption<>("support.plotsquared-auto-trust", true);
 
     /**
